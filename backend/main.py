e556981dadbf1afc993a76baaf7e0c32eff053dc
harshittiwari3944 <harshittitwari980len@gmail.com>
2026-06-03 12:08:32 +0530

    fix CORS for production

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://swaphoob.netlify.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
